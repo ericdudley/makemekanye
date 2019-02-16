01d85ae6f7877134bfa77b6ea6493ac40de7e6db
Robert Ellegate <rje6459@rit.edu>
2019-02-16 18:14:39 -0500

    Added dotenv package

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 from detect_face import detect_face
 
+from dotenv import load_dotenv
+load_dotenv()
+
 app = Flask(__name__)
 
 
@@ -9,11 +12,12 @@ def main():
     return render_template("main.html")
 
 
-@app.route("/faceify",["POST"])
+@app.route("/faceify",methods=["POST"])
 def faceify():
     base64_image = request.json
     Faces = detect_face(base64_image)
     print(Faces)
+    return(request.json)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
