062009829b728f8eefdb25ce1d410968127fc679
Robert Ellegate <rje6459@rit.edu>
2019-02-16 17:38:20 -0500

    Added backend stuff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import *
+from flask import Flask, render_template, request
+from detect_face import detect_face
 
 app = Flask(__name__)
 
@@ -8,10 +9,11 @@ def main():
     return render_template("main.html")
 
 
-@app.route("/faceify")
+@app.route("/faceify",["POST"])
 def faceify():
-    pass
-
+    base64_image = request.json
+    Faces = detect_face(base64_image)
+    print(Faces)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
