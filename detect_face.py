3a3e5973d4540e39c53c9f46e1ec8d358f884a68
Robert Ellegate <rje6459@rit.edu>
2019-02-16 16:53:51 -0500

    Changed detect_faces to use base64 string instead of file path

--- detect_face.py
+++ detect_face.py
@@ -1,29 +1,37 @@
+import base64
+from io import BytesIO
 from google.cloud import vision
 from google.cloud.vision import types
-import io
 
-def detect_face(path):
+
+def detect_face(string):
     client = vision.ImageAnnotatorClient()
 
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
-
+    content = BytesIO(base64.b64decode(string))
     image = types.Image(content=content)
     response = client.face_detection(image=image)
     faces = response.face_annotations
 
     # Names of likelihood from google.cloud.vision.enums
-    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLOKELY', 'POSSIBLE', 'LIKELY', 'VERY_LIKELY')
+    likelihood_name = (
+        "UNKNOWN",
+        "VERY_UNLIKELY",
+        "UNLOKELY",
+        "POSSIBLE",
+        "LIKELY",
+        "VERY_LIKELY",
+    )
 
-    print('Faces:')
+    print("Faces:")
 
     for face in faces:
-        print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
-        print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
-        print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
-        
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    for vertex in face.bounding_poly.vertices])
-        
-        print('face bounds: {}'.format(','.join(vertices)))
+        print("anger: {}".format(likelihood_name[face.anger_likelihood]))
+        print("joy: {}".format(likelihood_name[face.joy_likelihood]))
+        print("surprise: {}".format(likelihood_name[face.surprise_likelihood]))
 
+        vertices = [
+            "({},{})".format(vertex.x, vertex.y)
+            for vertex in face.bounding_poly.vertices
+        ]
+
+    print("face bounds: {}".format(",".join(vertices)))
