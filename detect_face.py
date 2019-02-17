52a9da71480f853f84b3f2c3cb165f2c0a55f19b
Robert Ellegate <rje6459@rit.edu>
2019-02-16 21:59:25 -0500

    Commenting out unused code

--- detect_face.py
+++ detect_face.py
@@ -5,19 +5,19 @@ from google.cloud import vision
 from google.cloud.vision import types
 
 
-def _select_fields(faces):
-    for face in faces:
-        face = {
-            "roll_angle": face.roll_angle,
-            "pan_angle": face.pan_angle,
-            "tilt_angle": face.tilt_angle,
-            "joy_likelihood": face.joy_likelihood,
-            "sorrow_likelihood": face.sorrow_likelihood,
-            "anger_likelihood": face.anger_likelihood,
-            "surprise_likelihood": face.surprise_likelihood,
-            "landmarks": face.landmarks
-        }
-    return faces
+# def _select_fields(faces):
+#     for face in faces:
+#         face = {
+#             "roll_angle": face.roll_angle,
+#             "pan_angle": face.pan_angle,
+#             "tilt_angle": face.tilt_angle,
+#             "joy_likelihood": face.joy_likelihood,
+#             "sorrow_likelihood": face.sorrow_likelihood,
+#             "anger_likelihood": face.anger_likelihood,
+#             "surprise_likelihood": face.surprise_likelihood,
+#             "landmarks": face.landmarks
+#         }
+#     return faces
 
 
 def detect_face(path):
@@ -31,7 +31,7 @@ def detect_face(path):
     response = client.face_detection(image=image)
     faces = response.face_annotations
 
-    faces = _select_fields(faces)
+    #faces = _select_fields(faces)
     return faces
 
 
@@ -42,27 +42,7 @@ if __name__ == "__main__":
 
     faces = detect_face(sys.argv[1])
 
-    # Names of likelihood from google.cloud.vision.enums
-    likelihood_name = (
-        "UNKNOWN",
-        "VERY_UNLIKELY",
-        "UNLOKELY",
-        "POSSIBLE",
-        "LIKELY",
-        "VERY_LIKELY",
-    )
-
     print("Faces:")
 
     for face in faces:
         print(face)
-        # print("anger: {}".format(likelihood_name[face.anger_likelihood]))
-        # print("joy: {}".format(likelihood_name[face.joy_likelihood]))
-        # print("surprise: {}".format(likelihood_name[face.surprise_likelihood]))
-
-        # vertices = [
-        #    "({},{})".format(vertex.x, vertex.y)
-        #    for vertex in face.bounding_poly.vertices
-        # ]
-
-        # print("face bounds: {}".format(",".join(vertices)))
